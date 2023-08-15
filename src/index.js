3c86e150ee5071484380d42371b0062d710b706a
anatonokao <povelitel.271@gmail.com>
2023-08-15 22:00:24 +0300

    feat: add morse decoder

--- src/index.js
+++ src/index.js
@@ -1,46 +1,70 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+   '.-': 'a',
+   '-...': 'b',
+   '-.-.': 'c',
+   '-..': 'd',
+   '.': 'e',
+   '..-.': 'f',
+   '--.': 'g',
+   '....': 'h',
+   '..': 'i',
+   '.---': 'j',
+   '-.-': 'k',
+   '.-..': 'l',
+   '--': 'm',
+   '-.': 'n',
+   '---': 'o',
+   '.--.': 'p',
+   '--.-': 'q',
+   '.-.': 'r',
+   '...': 's',
+   '-': 't',
+   '..-': 'u',
+   '...-': 'v',
+   '.--': 'w',
+   '-..-': 'x',
+   '-.--': 'y',
+   '--..': 'z',
+   '.----': '1',
+   '..---': '2',
+   '...--': '3',
+   '....-': '4',
+   '.....': '5',
+   '-....': '6',
+   '--...': '7',
+   '---..': '8',
+   '----.': '9',
+   '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+   const arrWordsDigital = expr.split('**********')
+
+   const arrWord = arrWordsDigital.map(w => {
+      let wordMorze = []
+
+      for (let i = 0; i < w.length; (i += 10)) {
+         let letterMorze = []
+         let letterDigital = w.slice(i, i + 10)
+
+         for (let i = 0; i < letterDigital.length; (i += 2)) {
+            let morzeSymbol = letterDigital.slice(i, i + 2)
+            morzeSymbol !== '00' && (morzeSymbol === '10' ? letterMorze.push('.') : letterMorze.push('-'))
+         }
+
+         letterMorze = letterMorze.join('')
+         wordMorze.push(letterMorze)
+      }
+
+      const word = wordMorze.map(s => MORSE_TABLE[s] ).join('')
+      return word
+   })
+   
+   return arrWord.join(' ')
 }
 
+
+
 module.exports = {
-    decode
+   decode
 }
